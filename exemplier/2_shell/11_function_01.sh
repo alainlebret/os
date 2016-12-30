be49b601d4779160f2c36958c6aecefaf943bdc7
Alain LEBRET <alain.lebret@ensicaen.fr>
2016-12-30 18:54:25 +0100

    Add files via upload

--- exemplier/2_shell/11_function_01.sh
+++ exemplier/2_shell/11_function_01.sh
@@ -10,9 +10,17 @@
 #
 # Copyright (C) 1995-2016 Alain Lebret (alain.lebret@ensicaen.fr)
 #
-# This work is licensed under a Creative Commons Attribution-ShareAlike 4.0
-# International License. You should have received a copy of the License.
-# If not, see https://creativecommons.org/licenses/by-sa/4.0/.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 # A simple script to show how to use functions. This one just displays a message.
