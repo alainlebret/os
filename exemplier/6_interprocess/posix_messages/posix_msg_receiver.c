bf3c273f0116c59e0d2977b1807343593281c5df
Alain LEBRET <alain.lebret@ensicaen.fr>
2017-04-26 11:55:15 +0200

    Add files via upload

---
+++ exemplier/6_interprocess/posix_messages/posix_msg_receiver.c
@@ -0,0 +1,104 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Maréchal Juin
+ * F-14050 Caen Cedex
+ *
+ * Unix System Programming Examples / Exemplier de programmation système Unix
+ * Chapter "Interprocess communication" / Chapitre "Communication interprocessus"
+ *
+ * Copyright (C) 1995-2016 Alain Lebret (alain.lebret@ensicaen.fr)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file posix_msg_receiver.c
+ * @author Alain Lebret
+ * @version	1.0
+ * @date 2016-11-01
+ */
+
+#include <fcntl.h>
+#include <mqueue.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#define EVER ;;
+
+/*
+ * Application using POSIX mqueue
+ * Compile using: gcc -lrt -o posix_msg_receiver posix_msg_receiver.c
+ */
+int main(int argc,char * argv[]){
+	mqd_t mq;
+	char * buffer;
+	struct mq_attr attr;
+	struct timeval heure, *recue;
+	int taille, nb_messages;
+	long int duree, duree_max, duree_min, somme_durees;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s nom_file_message\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	mq = mq_open(argv[1], O_RDONLY | O_CREAT, 0600, NULL);
+	if (mq == (mqd_t) -1) {
+		perror(argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	if (mq_getattr(mq, & attr) != 0) {
+		perror("mq_getattr"); exit(EXIT_FAILURE);
+	}
+
+	taille = attr.mq_msgsize;
+	buffer = malloc(taille);
+	if (buffer == NULL) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	recue = (struct timeval *) buffer;
+
+	for (EVER) {
+		nb_messages = 0;
+		duree_max = 0;
+		duree_min = -1;
+		somme_durees = 0;
+
+		do {
+			mq_receive(mq, buffer, taille, NULL);
+			gettimeofday(& heure, NULL);
+			duree  = heure.tv_sec - recue->tv_sec;
+			duree *= 1000000;
+			duree += heure.tv_usec - recue->tv_usec;
+			if (nb_messages > 0) { /* Ignore first message */
+				if (duree_max < duree) {
+					duree_max = duree;
+					if ((duree_min == -1) || (duree_min > duree)) {
+						duree_min = duree;
+						somme_durees += duree;
+					}
+				}
+			}
+			nb_messages ++;
+		} while (nb_messages < 100000); /* approximately 1 sec */
+
+		fprintf(stdout, "min =%3ld   max =%3ld moy=%5.1f\n",
+				  duree_min, duree_max, ((float) somme_durees) / (nb_messages - 1));
+	}
+	return EXIT_SUCCESS;
+}
---
+++ exemplier/6_interprocess/posix_messages/posix_msg_sender.c
@@ -0,0 +1,64 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Maréchal Juin
+ * F-14050 Caen Cedex
+ *
+ * Unix System Programming Examples / Exemplier de programmation système Unix
+ * Chapter "Interprocess communication" / Chapitre "Communication interprocessus"
+ *
+ * Copyright (C) 1995-2016 Alain Lebret (alain.lebret@ensicaen.fr)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file posix_msg_sender.c
+ * @author Alain Lebret
+ * @version	1.0
+ * @date 2016-11-01
+ */
+
+#include <fcntl.h>
+#include <mqueue.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#define EVER ;;
+
+/*
+ * Application using POSIX mqueue
+ * Compile using: gcc -lrt -o posix_msg_sender posix_msg_sender.c
+ */
+int main(int argc,char * argv[]) {
+	mqd_t mq;
+	struct timeval heure;
+
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s message_queue\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	mq = mq_open(argv[1], O_WRONLY | O_CREAT, 0600, NULL);
+	if (mq == (mqd_t) -1) {
+		perror(argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	for (EVER) {
+		gettimeofday(& heure, NULL);
+		mq_send(mq, (char *) & heure, sizeof(heure), 1);
+	}
+	return EXIT_SUCCESS;
+}
