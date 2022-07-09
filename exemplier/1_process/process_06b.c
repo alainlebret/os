be5c0d2bd45960637a7611bf396aec79c23d390f
Alain Lebret <24602011+alainlebret@users.noreply.github.com>
2022-07-09 14:19:49 +0200

    Add files via upload

--- exemplier/1_process/process_06b.c
+++ exemplier/1_process/process_06b.c
@@ -48,7 +48,7 @@
  * It displays the given error message, then exits.
  * @param msg The error message to display before exiting.
  */
-void handle_fatal_error_and_exit(char *msg)
+void handle_fatal_error_and_exit(const char *msg)
 {
     perror(msg);
     exit(EXIT_FAILURE);
@@ -101,7 +101,7 @@ int main(void)
 
     pid = fork();
     if (pid == -1) {
-        handle_fatal_error_and_exit("Error using fork().\n");
+        handle_fatal_error_and_exit("Error [fork()]: ");
     }
 
     if (pid > 0) {
